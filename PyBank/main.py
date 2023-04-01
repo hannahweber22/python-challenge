102279c228de9d5699ffa261333367ec6924fcee
Hannah <hannahw7298@gmail.com>
2023-04-01 00:15:38 -0500

    adding updates to Pybank - Pybank challenge produces answer to terminal

--- PyBank/main.py
+++ PyBank/main.py
@@ -19,8 +19,8 @@ with open(budgetdata_csv) as csvfile:
     for row in csvreader:
         date.append(row[0])
         split_date = row[0].split("-")
-        month_list = month.append(split_date[0])
-        year_list = year.append(split_date[1])
+        month.append(split_date[0])
+        year.append(split_date[1])
         profit_loss.append(row[1])
 
 #The total number of months included in the dataset
@@ -33,14 +33,48 @@ with open(budgetdata_csv) as csvfile:
             total_month_list.append(index)
             total_month_count = total_month_count + 1  
     
-    print(total_month_count)
+    print(f"Total months: {total_month_count}")
 
 #The net total amount of Profit/ Loss over the entire period
     #float each index in profit_loss then sum all of the floats
     profit_loss_total = sum(float(index) for index in profit_loss)
-    print(f"Profit/Loss Net Total Amount {profit_loss_total}")
+    print(f"Total: ${profit_loss_total}")
 
 #The changes in Proft/ Loss over the entire period, and the average of those changes
+    
+    #find the changes month to month and determine if they are profits or losses; additionally, add the changes to a list
+    change_list = []
+
+    for location in range(1, len(profit_loss)):
+        if profit_loss[location] < profit_loss[location - 1]:
+            loss = abs(float(profit_loss[location]) - float(profit_loss[location - 1])) * -1
+            change_list.append(loss)
+        else:
+            profit = abs(float(profit_loss[location]) - float(profit_loss[location - 1])) 
+            change_list.append(profit)
 
 
+    #find total change in profit_loss and determine if it is a profit or loss
+    if float(profit_loss[0]) > float(profit_loss[-1]):   
+        total_change = abs(float(profit_loss[0]) - float(profit_loss[-1])) * -1
+    else:
+        total_change = abs(float(profit_loss[0]) - float(profit_loss[-1])) 
 
+    #find average of changes over period dividing the change between the first and last profit_loss values by length of profit_loss changes
+    average_change = total_change/ len(change_list)
+    print(f"Average Change: ${average_change}")
+    
+  
+ #The greatest increase in profits (data and amount) over the entire period
+    great_increase_profit = max(change_list)
+    great_increase_index = change_list.index(great_increase_profit) + 1
+    great_increase_date = date[great_increase_index]
+
+    print(f"Greatest Increase in Profits: {great_increase_date} (${great_increase_profit})")
+
+#The greatest decrease in profits (date and amount) over the entire period
+    great_decrease_profit = min(change_list)
+    great_decrease_index = change_list.index(great_decrease_profit) + 1
+    great_decrease_date = date[great_decrease_index]
+
+    print(f"Greatest Decrease in Profits: {great_decrease_date} (${great_decrease_profit})")
