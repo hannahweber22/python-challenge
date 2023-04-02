7495a6c7f8cf72ca4ca127d3b84b9408b4bfbe07
Hannah <hannahw7298@gmail.com>
2023-04-01 19:22:28 -0500

    PyBank challenge script completion

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,3 +1,4 @@
+
 import os
 import csv
 
@@ -33,12 +34,10 @@ with open(budgetdata_csv) as csvfile:
             total_month_list.append(index)
             total_month_count = total_month_count + 1  
     
-    print(f"Total months: {total_month_count}")
-
 #The net total amount of Profit/ Loss over the entire period
     #float each index in profit_loss then sum all of the floats
     profit_loss_total = sum(float(index) for index in profit_loss)
-    print(f"Total: ${profit_loss_total}")
+    
 
 #The changes in Proft/ Loss over the entire period, and the average of those changes
     
@@ -62,19 +61,42 @@ with open(budgetdata_csv) as csvfile:
 
     #find average of changes over period dividing the change between the first and last profit_loss values by length of profit_loss changes
     average_change = total_change/ len(change_list)
-    print(f"Average Change: ${average_change}")
     
-  
  #The greatest increase in profits (data and amount) over the entire period
     great_increase_profit = max(change_list)
     great_increase_index = change_list.index(great_increase_profit) + 1
     great_increase_date = date[great_increase_index]
-
-    print(f"Greatest Increase in Profits: {great_increase_date} (${great_increase_profit})")
 
 #The greatest decrease in profits (date and amount) over the entire period
     great_decrease_profit = min(change_list)
     great_decrease_index = change_list.index(great_decrease_profit) + 1
     great_decrease_date = date[great_decrease_index]
 
-    print(f"Greatest Decrease in Profits: {great_decrease_date} (${great_decrease_profit})")
+#Create variables with analysis results and insert variables in a list to be looped through to print/ write results
+    #assign variables to analysis results to be printed and written
+    Finanical_Analysis = "Financial Analysis"
+    Dash = "----------------------------"
+    Total_Months = f"Total months: {total_month_count}"
+    Profit_Loss_Total = f"Total: ${profit_loss_total: .0f}"
+    Average_Change = f"Average Change: ${average_change: .2f}"
+    Greatest_Increase = f"Greatest Increase in Profits: {great_increase_date} (${great_increase_profit: .0f})"
+    Greatest_Decrease = f"Greatest Decrease in Profits: {great_decrease_date} (${great_decrease_profit: .0f})"
+
+    #create list of analysis for printer
+    analysis_list = [Finanical_Analysis, Dash, Total_Months, Profit_Loss_Total, Average_Change, Greatest_Increase, Greatest_Decrease]
+
+ #Print analysis results to terminal
+    #Loop through the analysis list and print analysis results to terminal   
+    for printer in analysis_list:
+        print(printer)
+
+#Print analysis to text file in analysis folder
+#create path to write the txt to 
+output_path = os.path.join("analysis", "main.txt")
+
+#open file and write to file
+with open(output_path, 'w') as file:
+    #loop through the analysis_list and write to file
+    for writer in analysis_list:
+        file.writelines(writer)
+        file.write("\n")
